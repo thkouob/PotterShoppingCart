8dab5d4e78ea3b3e9178cf2b9e4372ee0edfa543
Thkouob Chen <thkouob@gmail.com>
2016-03-09 01:46:03 +0800

    init

---
+++ PotterShoppingCart.Tests/CalculatedShoppingCartTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PotterShoppingCart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class CalculatedShoppingCartTests
+    {
+        [TestMethod()]
+        public void Test_1Book1()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and1Book2()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 1);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and1Book2and1Book3()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 1);
+            target.AddBook("book3", 1);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 270;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and1Book2and1Book3and1Book4()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 1);
+            target.AddBook("book3", 1);
+            target.AddBook("book4", 1);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 320;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and1Book2and1Book3and1Book4and1Book5()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 1);
+            target.AddBook("book3", 1);
+            target.AddBook("book4", 1);
+            target.AddBook("book5", 1);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 375;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and1Book2and2Book3()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 1);
+            target.AddBook("book3", 2);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 370;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Test_1Book1and2Book2and2Book3()
+        {
+            //arrange
+            CalculatedShoppingCart target = new CalculatedShoppingCart();
+            target.AddBook("book1", 1);
+            target.AddBook("book2", 2);
+            target.AddBook("book3", 2);
+
+            //act
+            var actual = target.CalculateTotalPrice();
+            //Assert
+
+            var expected = 460;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/CalculatedShoppingCart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class CalculatedShoppingCart
+    {
+        decimal oneBookPrice = 100;
+
+        private Dictionary<string, int> bookList = new Dictionary<string, int>();
+
+        public decimal CalculateTotalPrice() 
+        {
+            decimal sum = 0;
+            int[] booksCount = bookList.Values.OrderByDescending(x => x).ToArray();
+            
+            for (var bookSet = bookList.Count; bookSet >= 1; bookSet--)
+            {
+                int setCount = 0;
+                if (bookSet == bookList.Count)//第一筆
+                {
+                    setCount = booksCount[bookSet - 1];
+                }
+                else
+                {
+                    setCount = booksCount[bookSet - 1] - booksCount[bookSet];
+                }
+
+                //價格+=折扣*單價*數量差*書的種類數
+                sum += this.GetDisCount(bookSet) * setCount * bookSet * oneBookPrice;
+
+            }
+            return sum;
+        }
+
+        public void AddBook(string vol, int count) 
+        {
+            bookList.Add(vol, count);
+        }
+
+        private decimal GetDisCount(int bookSet) 
+        {
+            decimal discount = 0;
+            switch (bookSet) 
+            {
+                case 2:
+                    discount = 0.95m;
+                    break;
+                case 3:
+                    discount = 0.9m;
+                    break;
+                case 4:
+                    discount = 0.8m;
+                    break;
+                case 5:
+                    discount = 0.75m;
+                    break;
+                default:
+                    discount = 1;
+                    break;
+            }
+
+            return discount;
+        }
+    }
+}
